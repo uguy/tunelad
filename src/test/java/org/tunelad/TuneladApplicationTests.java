28f19b12a7e3208caf3e5fb8875daf63a0cbdbb1
Hugues BRETIN <uguy@users.noreply.github.com>
2025-12-03 23:59:36 +0100

    chore(ui): fix spa filter (#651)
    * chore(ui): fix spa filter

--- src/test/java/org/tunelad/TuneladApplicationTests.java
+++ src/test/java/org/tunelad/TuneladApplicationTests.java
@@ -2,16 +2,17 @@ package org.tunelad;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.context.ImportTestcontainers;
 
 @Slf4j
 @SpringBootTest
+@ImportTestcontainers(TestContainerDefinitions.class)
 class TuneladApplicationTests {
 
-	@Test
-	void context_load() {
+    @Test
+    void context_load() {
 
-	}
+    }
 
 }
